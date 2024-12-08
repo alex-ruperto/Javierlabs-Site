9f49d883bbac2038e4d2836052d01dedbe2a34ee
Alex <alex.ruperto@javierlabs.com>
2024-12-08 16:28:19 -0500

    made CSS styling more consistent

--- frontend/javierlabs-frontend/src/pages/About.tsx
+++ frontend/javierlabs-frontend/src/pages/About.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from 'react';
 import '../styles/About.css';
+import {Navbar} from '../components/Navbar';
 
 export function About(): ReactElement {
     return (
         <div className="about">
+            <Navbar />
+
+            <div className="about-content-container">
+                <image>
+
+                </image>
+            </div>
+
 
         </div>
     );
