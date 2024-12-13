0658fe209e840ef92464d9f647e81249d47ef330
Alex Ruperto <alex.ruperto@vanderlande.com>
2024-12-13 12:03:50 -0500

    Made the home page mobile friendly. Made some adjustments to about.css for consistency of the typewriter effect. More mobile-friendly design across the board.

--- frontend/javierlabs-frontend/src/pages/socials page/Socials.tsx
+++ frontend/javierlabs-frontend/src/pages/socials page/Socials.tsx
@@ -12,12 +12,12 @@ export function Socials(): ReactElement {
             <div className="socials-content-container">
                 <div className="socials-items">
                     <div className="socials-item">
-                        <img src={github} alt="GitHub"/>
-                        <a href="https://github.com/alex-ruperto">https://github.com/alex-ruperto</a>
-                    </div>
-                    <div className="socials-item">
                         <img src={linkedin} alt="LinkedIn"/>
                         <a href="https://www.linkedin.com/in/alex-j-ruperto/">https://www.linkedin.com/in/alex-j-ruperto/</a>
+                    </div>
+                    <div className="socials-item">
+                        <img src={github} alt="GitHub"/>
+                        <a href="https://github.com/alex-ruperto">https://github.com/alex-ruperto</a>
                     </div>
                     <div className="socials-item">
                         <img src={email} alt="Email"/>
